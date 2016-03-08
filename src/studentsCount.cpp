5e042bc16162c4eaa0d1f9c9698b4a7c62060883
vasishtaballa <vasishtaballa@missionrnd.com>
2016-03-08 23:16:31 +0530

    Submit Commit

--- src/studentsCount.cpp
+++ src/studentsCount.cpp
@@ -16,5 +16,19 @@ NOTES:
 #include <stdio.h>
 
 void * studentsCount(int *Arr, int len, int score, int *lessCount, int *moreCount) {
+	if (Arr == NULL)
+		return NULL;
+	if (len < 0 || score < 0)
+		return NULL;
+	int i;
+	*lessCount = 0;
+	*moreCount = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (Arr[i]>score)
+			*moreCount = *moreCount + 1;
+		else if (Arr[i] < score)
+			*lessCount = *lessCount + 1;
+	}
 	return NULL;
 }
