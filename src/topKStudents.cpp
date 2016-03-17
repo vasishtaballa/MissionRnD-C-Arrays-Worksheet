77bd4c201dc62709021350eedba738c37f509d67
vasishtaballa <vasishtaballa@missionrnd.com>
2016-03-18 00:26:55 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -15,12 +15,73 @@ NOTES:
 
 #include <iostream>
 #include <malloc.h>
+void * mergearray(struct student *students, int i, int j);
+void * dividing(struct student *students, int i, int j);
 
 struct student {
 	char *name;
 	int score;
 };
 
-struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+struct student ** topKStudents(struct student *students, int len, int K) 
+{
+	if (students == NULL)
+		return NULL;
+	if (len < 0)
+		return NULL;
+	if (K < 0)
+		return NULL;
+	if (K == 0)
+		return NULL;
+	if (K>len)
+		K = len;
+	int i, j;
+	dividing(students, 0, len - 1);
+	struct student **result = (student **)malloc(sizeof(student)*K);
+	for (int i = 0; i < K; i++)
+	{
+		result[i] = &students[i];
+	}
+	return result;
+}
+
+void * dividing(struct student *students, int i, int j)
+{
+	int k, mid = 0;
+	if (i >= j)
+		return students;
+	mid = (i + j) / 2;
+	dividing(students, i, mid);
+	dividing(students, mid + 1, j);
+	mergearray(students, i, j);
+	return students;
+}
+void * mergearray(struct student *students, int i, int j)
+{
+	int k, mid, l, p;
+	struct student b[100];
+	p = i;
+	mid = (i + j) / 2;
+	k = mid + 1;
+	l = i;
+	while (i <= mid && k <= j)
+	{
+		if (students[i].score >= students[k].score)
+			b[l++] = students[i++];
+		else
+			b[l++] = students[k++];
+	}
+	if (i > mid)
+	{
+		for (; k <= j;)
+			b[l++] = students[k++];
+	}
+	else if (k > j)
+	{
+		for (; i <= mid;)
+			b[l++] = students[i++];
+	}
+	for (l = p; l <= j; l++)
+		students[l] = b[l];
+	return students;
 }
